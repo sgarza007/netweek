1722a31fec0cb2770fd3d53dc9d9048ca0d0192a
Saul Garza <saulgarza@sauls-macbook-air.local>
2018-05-02 13:33:50 -0700

    Initial Commit

---
+++ Netweek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Netweek
+//
+//  Created by Saul Garza on 5/2/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
